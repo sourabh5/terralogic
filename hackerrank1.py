c176ac428788980ccbcb0023f57e780b727fdac2
sourabh5 <sourabh.agrawal.94@gmail.com>
2018-10-22 18:58:40 +0530

    hackerrank file added

--- hackerrank1.py
+++ hackerrank1.py
@@ -7,7 +7,7 @@ numCust = int(raw_input())
 income = 0
 
 for i in range(numCust):
-    size,price =map(int,raw_input().split()))
+    size,price =map(int,raw_input().split())
     if shoes[size]:
         income +=price
         shoes[size] -=1
